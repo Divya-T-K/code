ab382751a46b9b84626a568ccd69d20e984b4572
Divya-T-K <kamalavenidivya28@gmail.com>
2025-06-06 03:11:51 +0530

    added javascript as new

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log(5=='5');
+console.log(5==='5');
+let i=0;
+do{
+    i++;
+    console.log(i);
+}while(i<6)
+ function greet(name,age){/*parameter*/
+    return "Hello I'm "+name+ " and I'm a "+age+' girl';
+ }
+ console.log(greet('Divya',420));/*argument*/
+ function gree(name,age){/*parameter*/
+    return `Hello I'm ${name} and my frds are ${age}`
+ }
+ console.log(gree('Lusu',420));/*argument*/
+ function add(one,two){
+    return one+two;
+ }console.log(add(4,6));
+ 
